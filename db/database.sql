7a91d98c536a1a2462128017986e65420a5dd636
Zetx <88859552+zetCodex@users.noreply.github.com>
2022-01-03 18:20:07 +0200

    Add files via upload

---
+++ db/database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE inbex;
+CREATE TABLE website (
+    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    FirstName varchar(255),
+    LastName varchar(255),
+    Adress varchar(255),
+    PhoneNumber varchar(50),
+    Password varchar(255),
+    FileName varchar(100)
+);
