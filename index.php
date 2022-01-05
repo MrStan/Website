45b8f02fdc351a52e552494a2c46d3c007e379b9
Zet <88859552+zetCodex@users.noreply.github.com>
2022-01-05 09:49:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 $type = $_FILES['fileToUpload']['type'];
 //Check if the database is compatible.
-require('connect.php');
+require('db/connect.php');
 
 // check type of file.
 if($imageFileType != "img" && $imageFileType != "jpeg" && $imageFileType != "jpg" && $imageFileType != "gif" && $imageFileType != "png") {
